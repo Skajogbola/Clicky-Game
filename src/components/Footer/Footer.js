cbc9ba2ab4f0fbfe67ee4f21750728a6864cf76c
Simmie <skajogbola@users.noreply.github.com>
2019-06-20 16:14:16 -0400

    created footer and jumbotron folders

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<header className = "footer">
+		<p>GAMe CREATED BY SIMMIE</p>
+	</header>
+);
+
+export default Footer;
