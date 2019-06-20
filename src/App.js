4b6857f5a9a4c7b43f5a6f5db1f31b1860880572
Simmie <skajogbola@users.noreply.github.com>
2019-06-20 15:59:30 -0400

    Created the components folder

---
+++ src/App.js
@@ -0,0 +1,10 @@
+//imports dependencies and files
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import vegetables from "./vegetables.json";
+import "./App.css";
+
+export default App;
