fc86885d883090c0b0d3670af2d970407ddb1479
Simmie <skajogbola@users.noreply.github.com>
2019-06-22 13:47:30 -0400

    fixed the bugs in navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,20 @@
 //sets up the reusable Navbar component
-import React, { Component } from "react";
+import React from "react";
 import "./Navbar.css";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-default navbar-fixed-top">
-         <ul>
-          <li className="itemOne">Test Your Memory</li>
-          <li className="itemTwo">Status: {this.props.status}</li>
-          <li className="itemThree">Score: {this.props.score}</li>
-          <li className="itemFour">Top Score: {this.props.topscore}</li>
-        </ul>
-      </nav>
-    );
-  }
-}
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="brand">
+          <a href="/">Test Your Memory</a>
+        </li>
+        <li className="brand1">{props.status}</li>
+        <li className="brand2">Score: {props.score} | Top Score: {props.topScore}</li>
+      </ul>
+    </nav>
+  )
+}     
+   
 
 export default Navbar;
