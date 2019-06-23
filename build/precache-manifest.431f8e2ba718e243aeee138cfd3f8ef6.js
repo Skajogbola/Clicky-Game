cc43c7bd61566da5752662771c63e4163f7ebcd3
Simmie <skajogbola@users.noreply.github.com>
2019-06-22 20:13:13 -0400

    fixing bugs in navbar.js file

--- build/precache-manifest.431f8e2ba718e243aeee138cfd3f8ef6.js
+++ build/precache-manifest.431f8e2ba718e243aeee138cfd3f8ef6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7d3786693a10efe67178c8588e76ed5",
+    "revision": "b47d3fbb49a5e444e7b6d0e76a4a0c50",
     "url": "/Clicky-Game/index.html"
   },
   {
-    "revision": "731c794a27d725e519ad",
+    "revision": "316fea8b6ec6a64b10f2",
     "url": "/Clicky-Game/static/css/main.a1b07dc6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "731c794a27d725e519ad",
-    "url": "/Clicky-Game/static/js/main.78ab80bf.chunk.js"
+    "revision": "316fea8b6ec6a64b10f2",
+    "url": "/Clicky-Game/static/js/main.672026dc.chunk.js"
   },
   {
     "revision": "fab82da4d9420c34bc57",
